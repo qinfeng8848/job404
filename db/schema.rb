bde1991776ab4903af0bf588c7b17137f204d320
qinfeng8848 <q884822@gmail.com>
2018-07-31 09:07:30 +0800

    完成job的CURD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_073112) do
+ActiveRecord::Schema.define(version: 2018_07_30_075158) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
